6bf55637324a33d02d1f3d9c7e562e7303bbd249
EugeneGoode <zhenyahudenko@gmail.com>
2021-11-05 18:57:44 +0200

    dfghdfgh

---
+++ build/precache-manifest.2c3645c8e92b9787ee104091ac08f1c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bed1c83acde982d09d5a04cfeb6595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d51ecbe86b15e9a8914",
+    "url": "/static/css/main.6560c9ca.chunk.css"
+  },
+  {
+    "revision": "eb5428dbf24a874e1f47",
+    "url": "/static/js/2.1195147c.chunk.js"
+  },
+  {
+    "revision": "9d51ecbe86b15e9a8914",
+    "url": "/static/js/main.102e59af.chunk.js"
+  },
+  {
+    "revision": "e52355ce7cf88029702e",
+    "url": "/static/js/runtime-main.0d1674f1.js"
+  }
+]);
